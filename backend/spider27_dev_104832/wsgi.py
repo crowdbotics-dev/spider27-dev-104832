59ef37c3ccf0dbdd2eeecc7575dc0cd8667bd854
Crowdbotics <engineering@crowdbotics.com>
2023-07-27 07:06:31 +0000

    Initial commit.

---
+++ backend/spider27_dev_104832/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for spider27_dev_104832 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spider27_dev_104832.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spider27-dev-104832",
+  url: "https://spider27-dev-104832.botics.co",
+  api: "https://spider27-dev-104832.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
